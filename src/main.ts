492066721a9339ab20b543a6b79b24cfdc92dc40
lopesboa <lopes.boa@sanar.com>
2023-09-08 19:13:37 -0300

    chore: add class[validator, transform]

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,23 @@
-import { NestFactory } from '@nestjs/core';
 import helmet from 'helmet';
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+
 import { AppModule } from './app.module';
+import { AppLoggerAdapter } from '@/common/Logger/infrastructure/adapters';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useLogger(app.get(AppLoggerAdapter));
   app.enableCors();
   app.use(helmet());
   app.setGlobalPrefix('v1');
-  await app.listen(process.env.PORT);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      transformOptions: { enableImplicitConversion: true },
+    }),
+  );
+  await app.listen(process.env.PORT || 4001);
 }
 bootstrap();
