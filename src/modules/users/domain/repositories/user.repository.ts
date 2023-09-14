c3235a92a1facb783c8d1f69493add267fdc6014
lopesboa <lopes.boa@sanar.com>
2023-09-14 12:00:41 -0300

    refactor: update user repository types

--- src/modules/users/domain/repositories/user.repository.ts
+++ src/modules/users/domain/repositories/user.repository.ts
@@ -3,6 +3,6 @@ import { UserEntity } from '../entities/user.entity';
 export type UserRepository = {
   save(user: UserEntity): Promise<void>;
   update(user: UserEntity): Promise<void>;
-  list(): Promise<UserEntity[]>;
   get(userId: string): Promise<UserEntity>;
+  findOne(params): Promise<UserEntity>;
 };
