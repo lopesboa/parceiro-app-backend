464038cf79d964050ff725927a773040121e26c0
lopesboa <lopes.boa@sanar.com>
2023-09-16 11:54:46 -0300

    feat: create realms database repository implementation

--- src/modules/realms/realms.module.ts
+++ src/modules/realms/realms.module.ts
@@ -1,1 +1,9 @@
+import { Module } from '@nestjs/common';
+import { RealmsDatabaseRepository } from './infrastructure/realms-database-repositoty';
+
+@Module({
+  providers: [
+    { provide: 'RealmsRepository', useClass: RealmsDatabaseRepository },
+  ],
+})
 export class RealmsModule {}
