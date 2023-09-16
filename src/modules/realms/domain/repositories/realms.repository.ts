ec20fa47d3f6e3637416b9ea9743384b41b2b601
lopesboa <lopes.boa@sanar.com>
2023-09-16 13:44:21 -0300

    feat: create query to get all realms & remove end connection

--- src/modules/realms/domain/repositories/realms.repository.ts
+++ src/modules/realms/domain/repositories/realms.repository.ts
@@ -4,5 +4,6 @@ export type RealmsRepository = {
   save(realms: Realms): Promise<void>;
   update(realm: Realms): Promise<void>;
   findOne(realmId: string): Promise<Realms>;
+  getAll(limit: number, offset: number): Promise<[Realms]>;
   delete(realmId: string): Promise<void>;
 };
