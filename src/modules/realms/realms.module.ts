bf51aa0ebd53f2a5f9f97426eb7829648110ea38
lopesboa <lopes.boa@sanar.com>
2023-09-16 13:13:02 -0300

    feat: create realms controller

--- src/modules/realms/realms.module.ts
+++ src/modules/realms/realms.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { RealmsDatabaseRepository } from './infrastructure/realms-database-repositoty';
-
+import { RealmsController } from './presentation';
+import { CreateRealmUseCaseImplementation } from './application';
 @Module({
   providers: [
     { provide: 'RealmsRepository', useClass: RealmsDatabaseRepository },
+    {
+      provide: 'CreateRealmUseCase',
+      useClass: CreateRealmUseCaseImplementation,
+    },
   ],
+  controllers: [RealmsController],
 })
 export class RealmsModule {}
