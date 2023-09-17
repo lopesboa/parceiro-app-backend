3a22319cfdbf6e82520aa91ef5509e1b855abb18
lopesboa <lopes.boa@sanar.com>
2023-09-16 21:13:35 -0300

    feat: add roles entity

---
+++ src/modules/roles/domain/entities/roles.entity.ts
@@ -0,0 +1,10 @@
+import { UUID } from 'crypto';
+
+export class Roles {
+  role_id?: UUID;
+  name: string;
+  application_id: UUID;
+  permissions: string[];
+  created_at?: Date;
+  updated_at?: Date;
+}
