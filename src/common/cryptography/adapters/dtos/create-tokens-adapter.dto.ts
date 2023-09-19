c321727679a6d6d6de814511d199566615eacaac
lopesboa <lopes.boa@sanar.com>
2023-09-19 20:32:08 -0300

    feat: create create tokens dtos & types

---
+++ src/common/cryptography/adapters/dtos/create-tokens-adapter.dto.ts
@@ -0,0 +1,46 @@
+import { IsArray, IsNotEmpty, IsString, IsUUID } from 'class-validator';
+import { UUID } from 'crypto';
+
+export class GetTokensInputDTO {
+  @IsUUID()
+  @IsNotEmpty()
+  userId: UUID;
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+  @IsNotEmpty()
+  @IsArray({ each: true })
+  permissions: string[];
+}
+
+export class RefreshTokenInputDTO {
+  @IsUUID()
+  @IsNotEmpty()
+  userId: UUID;
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+  @IsNotEmpty()
+  @IsArray({ each: true })
+  permissions: string[];
+  @IsNotEmpty()
+  @IsString()
+  refreshToken: string;
+  @IsNotEmpty()
+  @IsString()
+  tokenToRefresh: string;
+}
+
+export class CreateHashedTokensInputDTO {
+  @IsNotEmpty()
+  @IsString()
+  refreshToken: string;
+  @IsNotEmpty()
+  @IsString()
+  accessToken: string;
+}
+
+export class TokensResponseDTO {
+  accessToken: string;
+  refreshToken: string;
+}
