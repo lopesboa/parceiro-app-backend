922c8b218a0b4c69053b3e1aa4cf9da7ead26199
lopesboa <lopes.boa@sanar.com>
2023-09-23 16:51:16 -0300

    feat: create access token strategy

---
+++ src/modules/auth/application/strategy/access-token.strategy.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+type JwtPayload = {
+  sub: string;
+  name: string;
+};
+
+@Injectable()
+export class AccessTokenStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: process.env.JWT_ACCESS_SECRET,
+    });
+  }
+
+  validate(payload: JwtPayload) {
+    //TODO: return the user data from userRepository
+    return { userId: payload.sub, name: payload.name };
+  }
+}
