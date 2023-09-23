d8a99408ce577faae9a96a5625c65daf26deab51
lopesboa <lopes.boa@sanar.com>
2023-09-23 16:53:35 -0300

    refactor: crete validate user dto

--- src/modules/auth/application/dtos/login-reponse.dto.ts
+++ src/modules/auth/application/dtos/login-reponse.dto.ts
@@ -1,4 +1,15 @@
+import { UUID } from 'crypto';
+
 export class LoginResponseDTO {
   accessToken: string;
   refreshToken: string;
+}
+
+export class ValidateUserDTO {
+  userId: UUID;
+  firstName: string;
+  lastName: string;
+  email: string;
+  applicationId: string;
+  tokenId: string;
 }
