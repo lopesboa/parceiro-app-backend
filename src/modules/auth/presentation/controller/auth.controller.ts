bef8e60e96e0699bf54fb1793b75270aa7b11074
lopesboa <lopes.boa@sanar.com>
2023-09-23 17:07:58 -0300

    feat: implement guards & service in auth controller

--- src/modules/auth/presentation/controller/auth.controller.ts
+++ src/modules/auth/presentation/controller/auth.controller.ts
@@ -1,15 +1,44 @@
-import { Body, Controller, Inject, Post } from '@nestjs/common';
-import { SignInUseCase } from '../../application/types';
-import { LoginResponseDTO, SignInInputDTO } from '../../application/dtos';
+import {
+  Controller,
+  Get,
+  Inject,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
+
+import {
+  LocalAuthGuard,
+  AccessTokenGuard,
+  RefreshTokenGuard,
+} from '../../application/guards';
+import { LoginService } from '../../application/types';
 
 @Controller('auth')
 export class AuthController {
   constructor(
-    @Inject('SignInUseCase') private readonly signInUseCase: SignInUseCase,
+    @Inject('LoginService') private readonly loginService: LoginService,
   ) {}
 
+  @UseGuards(LocalAuthGuard)
   @Post('login')
-  async login(@Body() loginDto: SignInInputDTO): Promise<LoginResponseDTO> {
-    return this.signInUseCase.execute(loginDto);
+  async login(@Request() req) {
+    return this.loginService.login(req.user);
+  }
+
+  @Get('logout')
+  @UseGuards(AccessTokenGuard)
+  async logout(@Request() req) {
+    console.log(req.user);
+    // this.authenticationService.logout(req.user['sub']);
+  }
+
+  @UseGuards(RefreshTokenGuard)
+  @Get('refresh')
+  refreshTokens(@Request() req) {
+    const userId = req.user['sub'];
+    const refreshToken = req.user['refreshToken'];
+    console.log(refreshToken, userId);
+    // return this.authenticationService.refreshTokens(userId, refreshToken);
   }
 }
