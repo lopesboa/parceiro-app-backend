60cb3c11199840634b4e8807031c51c92fd37cef
lopesboa <lopes.boa@sanar.com>
2023-10-01 00:47:03 -0300

    feat: sent event after user is created

--- src/modules/users/user.module.ts
+++ src/modules/users/user.module.ts
@@ -1,14 +1,17 @@
 import { Module } from '@nestjs/common';
+import { EventEmitter2 } from '@nestjs/event-emitter';
+
+import { CryptographyModule } from '@/common';
 import { UserRepositoryImplementation } from './infrastructure';
 import { CreateUser } from './application/usecase/create-user.usecase';
 import { UserController } from './presentation/controller/user.controller';
-import { CryptographyModule } from '@/common';
 
 @Module({
   imports: [CryptographyModule],
   providers: [
     { provide: 'UserRepository', useClass: UserRepositoryImplementation },
     { provide: 'CreateUserUseCase', useClass: CreateUser },
+    { provide: 'EventEmitter', useExisting: EventEmitter2 },
   ],
   controllers: [UserController],
   exports: [
