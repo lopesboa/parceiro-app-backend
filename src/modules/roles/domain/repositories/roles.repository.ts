1bcd6e2e98213cfac9393589f21e51c4d413faaa
lopesboa <lopes.boa@sanar.com>
2023-10-08 22:09:58 -0300

    refactor: change repository contracts

--- src/modules/roles/domain/repositories/roles.repository.ts
+++ src/modules/roles/domain/repositories/roles.repository.ts
@@ -1,9 +1,10 @@
+import { UUID } from 'crypto';
 import { Roles } from '../entities';
 
 export type RolesRepository = {
   save(roles: Roles): Promise<void>;
   update(roles: Roles): Promise<void>;
-  findOne(roleId: string): Promise<Roles>;
+  findOne(roleId: string, applicationId: UUID): Promise<Roles>;
   getAll(limit: number, offset: number): Promise<[Roles]>;
   delete(roleId: string): Promise<void>;
 };
