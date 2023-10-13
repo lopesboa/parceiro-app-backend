84d48b8e42a7a53a70d137c8f4350ef19cf2418b
lopesboa <lopes.boa@sanar.com>
2023-10-13 11:11:16 -0300

    feat: create AssignRoleToUserJob implementation

--- src/modules/roles/infrastructure/index.ts
+++ src/modules/roles/infrastructure/index.ts
@@ -1,4 +1,6 @@
 export * from './create-role-job';
 export * from './role-created-listener';
+export * from './assign-role-to-user-job';
 export * from './roles-database-repository';
+export * from './assign-role-to-user-listener';
 export * from './user-to-role-database-repository';
