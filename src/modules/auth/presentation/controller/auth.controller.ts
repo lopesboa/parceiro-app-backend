93eece42d9efc50253f82933afd63adff6e63d90
lopesboa <lopes.boa@sanar.com>
2023-10-14 13:38:58 -0300

    feat: use rbac in logout & refresh routes

--- src/modules/auth/presentation/controller/auth.controller.ts
+++ src/modules/auth/presentation/controller/auth.controller.ts
@@ -8,7 +8,14 @@ import {
 } from '@nestjs/common';
 
 import { LogOutService, LoginService } from '../../application/types';
-import { AccessTokenGuard, LocalAuthGuard, RefreshTokenGuard } from '../guards';
+import {
+  AccessTokenGuard,
+  LocalAuthGuard,
+  RefreshTokenGuard,
+  RolesGuard,
+} from '../guards';
+import { Roles } from '../decorators';
+import { ALL_PERMISSIONS } from '@/common/config';
 
 @Controller('auth')
 export class AuthController {
@@ -23,13 +30,15 @@ export class AuthController {
     return this.loginService.login(req.user);
   }
 
+  @Roles(...ALL_PERMISSIONS)
+  @UseGuards(AccessTokenGuard, RolesGuard)
   @Get('logout')
-  @UseGuards(AccessTokenGuard)
   async logout(@Request() req) {
     await this.logOutService.logout(req.user['userId']);
   }
 
-  @UseGuards(RefreshTokenGuard)
+  @Roles(...ALL_PERMISSIONS)
+  @UseGuards(RefreshTokenGuard, RolesGuard)
   @Get('refresh')
   refreshTokens(@Request() req) {
     const userId = req.user['sub'];
