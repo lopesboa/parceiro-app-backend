d0e8a8acf784249fd2dc72fef5aa20e2c4afec68
lopesboa <lopes.boa@sanar.com>
2023-10-21 22:17:57 -0300

    feat: add route to list user info

--- src/modules/users/presentation/controller/user.controller.ts
+++ src/modules/users/presentation/controller/user.controller.ts
@@ -1,9 +1,26 @@
-import { Body, Controller, Inject, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Inject,
+  Post,
+  UseGuards,
+  Request,
+} from '@nestjs/common';
 
-import { CreateUserUseCase } from '../../application/types';
+import {
+  CreateUserUseCase,
+  ListUserInfoUseCase,
+} from '../../application/types';
 import { CreateUserInputDTO } from '../../application/dtos';
 import { AddRoleToUserUseCase } from '@/modules/roles/application/types';
 import { AddRoleToUserInputDTO } from '@/modules/roles/application/dtos';
+import {
+  AccessTokenGuard,
+  Roles,
+  RolesGuard,
+} from '@/modules/auth/presentation';
+import { ALL_PERMISSIONS } from '@/common/config';
 
 @Controller('users')
 export class UserController {
@@ -12,6 +29,8 @@ export class UserController {
     private readonly createUserUseCase: CreateUserUseCase,
     @Inject('AddRoleToUserUseCase')
     private readonly addRoleToUserUseCase: AddRoleToUserUseCase,
+    @Inject('ListUserInfoUseCase')
+    private readonly listUserInfoUseCase: ListUserInfoUseCase,
   ) {}
 
   @Post('register')
@@ -22,5 +41,15 @@ export class UserController {
   @Post('roles')
   assignRoleToUser(@Body() data: AddRoleToUserInputDTO) {
     this.addRoleToUserUseCase.execute(data);
+  }
+
+  @Roles(...ALL_PERMISSIONS)
+  @UseGuards(AccessTokenGuard, RolesGuard)
+  @Get('info')
+  getUserInformation(@Request() req) {
+    return this.listUserInfoUseCase.execute({
+      userId: req.user['userId'],
+      applicationId: req.user['applicationId'],
+    });
   }
 }
